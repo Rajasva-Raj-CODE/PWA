6fcaad6fe5f75df5a14bb483c8920062d92adbf2
Rajasva-Raj-CODE <rajasvarajshrivastava@gmail.com>
2025-09-19 14:52:54 +0530

    icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,6 +27,9 @@ export default function RootLayout({
        <head>
         <link rel="manifest" href="/manifest.json" />
         <link rel="icon" href="/icon-192.png" />
+        <link rel="apple-touch-icon" href="/icon-192.png" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
         <meta name="theme-color" content="#000000" />
       </head>
       <body
